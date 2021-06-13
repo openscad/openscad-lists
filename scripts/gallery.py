40f7231dbeaa5b7fd348f7c154ea6cc4f23f5518
Torsten Paul <Torsten.Paul@gmx.de>
2021-06-13 18:23:22 +0200

    Add simple converter script from yaml to json. (#1)

---
+++ scripts/gallery.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+import yaml
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help = 'input file')
+parser.add_argument('--output', '-o', required = True, help = 'output file')
+args = parser.parse_args()
+
+with open(sys.argv[1], 'r') as file:
+    data = yaml.safe_load(file);
+    with open(args.output, 'w') as output:
+        json.dump(data, output, indent=4)
